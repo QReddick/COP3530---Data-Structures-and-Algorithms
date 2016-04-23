ff1bb9499473665f54b99d44d6d7302caa61d1b4
Quinton Reddick <qreddick@ufl.edu>
2016-04-22 21:35:58 -0400

    updated main.cpp with code for LIS
    wrote the longest increasing subsequence for the magi order

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -6,8 +6,47 @@
 #include <string>
 
 
-int LongestIncreasingSubsequence(int arr[], int n) {
-	// n is magiCount
+int LIS_Helper(int arr[], int arr_size, int *result_size) {
+	
+	// if the size of the array is only 1, it's resulting subsquence is only 1
+	if (n == 1) {
+		return 1;
+	}
+
+	// result_ending is the length of the LIS ending with arr[arr_size - 1]
+	int result, result_ending = 1;
+
+
+	// rescursively get all LIS ending with arr[0] to arr[arr_size - 1]
+	for (int i = 1; i < arr_size; i++) {
+		res = LongestIncreasingSubsequence(arr, i, result_size);
+
+		if (arr[i-1] < arr[arr_size - 1] && result + 1 > result_ending) {
+			result_ending = result + 1;
+		}
+	} 
+
+	/*Compare result_ending with the overal result_size
+		update the overall result_size if needed */
+	if (*result_size < result_ending) {
+		*result_size = result_ending;
+	}
+
+	// return length of LIS ending with arr[arr_size - 1]
+	return result_ending;
+}
+
+int LIS(int arr[], int arr_size) {
+
+	//res will be the size of the longest subsquence found
+	int res = 1;
+
+	// find the longest subsequence
+	LIS_Helper(arr, n, &res);
+
+
+	// return res
+	return res;
 }
 
 int main(int argc, char** argv)
